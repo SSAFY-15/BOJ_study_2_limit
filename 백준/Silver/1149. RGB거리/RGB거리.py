67cae24f8649017dd2a803067948df3f5433e9a0
2_limit <tidhtidhdldl@naver.com>
2026-03-10 01:38:40 +0000

    Update 2_limit BOJ solutions

---
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+mat = [list(map(int, input().split())) for _ in range(N)]
+
+dp = [[0] * 3 for _ in range(N)]
+
+dp[0] = mat[0]
+
+for i in range(1, N):
+    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + mat[i][0]
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + mat[i][1]
+    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + mat[i][2]
+
+print(min(dp[N-1]))
