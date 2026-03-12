2b544076d92eebc8e6f56020c40a2ed58d957acb
2_limit <tidhtidhdldl@naver.com>
2026-03-12 06:35:49 +0000

    Update 2_limit BOJ solutions

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,17 @@
+N, K = map(int, input().split())
+
+mat = [list(map(int, input().split())) for _ in range(N)]
+
+dp = [[0] * (K + 1) for _ in range(N + 1)]
+
+for i in range(1, N + 1):
+    wi, vi = mat[i - 1]
+    for w in range(K + 1):
+        if w < wi:
+            dp[i][w] = dp[i - 1][w]
+        else:
+            dp[i][w] = max(
+                dp[i - 1][w],
+                dp[i - 1][w - wi] + vi
+            )
+print(dp[N][K])
